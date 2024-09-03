c0a0872376c0f5a065b315f96972edba030bd646
Paul Ramsey <22893884+paulramsey@users.noreply.github.com>
2024-09-03 12:51:38 -0500

    Prep UI to support PSVs

--- gemini/sample-apps/genwealth/ui/src/app/app.component.ts
+++ gemini/sample-apps/genwealth/ui/src/app/app.component.ts
@@ -13,6 +13,13 @@ import { ProspectsComponent } from './prospects/prospects.component';
 import { ChatComponent } from './chat/chat.component';
 import { SnackBarErrorComponent } from './common/SnackBarErrorComponent';
 
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatDividerModule} from '@angular/material/divider';
+
+import {MatMenuModule} from '@angular/material/menu';
+
+
 
 @Component({
   selector: 'app-root',
@@ -27,6 +34,10 @@ import { SnackBarErrorComponent } from './common/SnackBarErrorComponent';
     InvestmentsComponent,
     ProspectsComponent,
     ChatComponent,
+    MatButtonToggleModule, 
+    MatCheckboxModule,
+    MatDividerModule,
+    MatMenuModule
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
@@ -40,6 +51,30 @@ export class AppComponent implements OnInit {
   ngOnInit() { 
     this.breakpointObserver
       .observe([Breakpoints.Handset])
-      .subscribe( _ => this.isSmallScreen = this.breakpointObserver.isMatched(Breakpoints.Handset) );
+      .subscribe( _ => this.isSmallScreen = this.breakpointObserver.isMatched(Breakpoints.Handset) )
+    this.selected = 'Admin';
   }
+
+  selected :string | undefined;
+
+  select(pText :string)
+  {
+    this.selected = pText
+  }
+
+  lookupIdByRole(role: string): number | undefined {
+    const roleMap: Map<string, number> = new Map([
+      ["Advisor (Paul Ramsey)", 1],
+      ["Advisor (Evelyn Sterling)", 2],
+      ["Advisor (Arthur Kensington)", 3],
+      ["Advisor (Penelope Wainwright)", 4],
+      ["Advisor (Sebastian Thorne)", 5],
+      ["Subscriber (Basic)", 0],
+      ["Subscriber (Intermediate)", 1],
+      ["Subscriber (Premium)", 2],
+    ]);
+  
+    return roleMap.get(role); 
+  }
+
 }
